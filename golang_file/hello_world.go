a8a79401be677d9813017fc7fd7846f338c9c6e4
huiyang865 <huiyang865@hotmail.com>
2020-11-07 20:35:54 +0800

    变量/常量

--- golang_file/hello_world.go
+++ golang_file/hello_world.go
@@ -2,6 +2,9 @@ package main // 首行必须是package，静态语言特性
 
 import "fmt" //双引号
 
+func test() (int, error) {
+	return 0, nil
+}
 
 // func 定义函数, 将主要逻辑放到main函数中，不需要手动调用main函数
 func main() {
@@ -10,7 +13,6 @@ func main() {
 	println("hello world!") // 打印一行，换行
 	// print("hello world!")　//　不换行
 	fmt.Println("hello world!") // 使用更多
-
 
 	// 变量的定义
 	// 静态语言的变量与动态语言变量定义差异较大，python中不需要定义变量
@@ -28,8 +30,43 @@ func main() {
 	// 多变量定义
 	var a, b, c int = 10, 20, 30
 	fmt.Println(a, b, c)
-}
 
+	// 匿名变量，go语言中，变量被定义后一定要使用，否则会报错
+	_, err := test()
+	if err != nil {
+		fmt.Println("函数调用成功")
+	}
+
+	// go中常量不可被修改
+	const PI = 3.1415926
+	r := 2.0
+	ci := 2 * PI * r
+	fmt.Println(ci)
+
+	const (
+		xx int = 16
+		yy
+		ss = "abc"
+		zz
+	)
+	fmt.Println(xx, yy, ss, zz)
+
+	// const常量的iota，常量计数器
+	// 1. iota只能再常量组中使用
+	// 2. 各个常量const组之间互相不干扰
+	// 3. 没有表达式的常量定义，复用上一行的表达式
+	// 4. 从第一行开始，iota逐行加1
+	const (
+		ca = iota
+		cb = 10 // 每一行iota加一
+		cc
+		cd = iota
+		ce
+		cf, ch = iota, iota // iota代表的是行数，不是常量个数
+		cj     = iota
+	)
+	fmt.Println(ca, cb, cc, cd, ce, cf, ch, cj)
+}
 
 // go run hello_world.go 执行脚本
 // go build hello_world.go　编译脚本
--- python_file/hello_world.py
+++ python_file/hello_world.py
@@ -1,9 +1,8 @@
 def main():
-    print('hello world!') # 默认换行
+    print('hello world!')  # 默认换行
 
-    print('hello', end='') # 不换行
+    print('hello', end='')  # 不换行
     print(' world!')
-
 
     # 变量的定义
     i = 40
@@ -12,6 +11,17 @@ def main():
     i = 'hello world!'
     print(i)
 
+    # 定义匿名变量
+    my_list = range(10)
+    for _, item_num in enumerate(my_list):
+        print(item_num)
+
+    # 常量定义，python中常量是可以被修改
+    PI = 3.1415926
+    r = 20
+    c = 2 * PI * r
+    print(c)
+
 
 if __name__ == "__main__":
-    main()　#需要手动调用
+    main()  # 需要手动调用
